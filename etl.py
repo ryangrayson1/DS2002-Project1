f57fdc5b6cd0de68257dca42d87956f53cbaff43
ryangrayson1 <rtg5xkh@virginia.edu>
2023-03-28 14:17:03 -0400

    final testing

--- etl.py
+++ etl.py
@@ -1,7 +1,5 @@
-import os
 import kaggle
 import pandas as pd
-import collections
 import pymysql
 import sqlalchemy
 
@@ -9,32 +7,32 @@ import sqlalchemy
 Setup steps to use this ETL processor:
 
 1. you must have a kaggle.json file in /.kaggle/kaggle.json your home directory
-this is the content needed for the file:
+the following line is the exact content needed for the file (file also provided):
 {"username":"ryangrayson","key":"ff71ca5843ee7c2766618b7faee5e6f9"}
 
 2. you must also have a mysql database connection set up at localhost with the following credentials:
-(or you can change the credentials in the code below)
+(or you can change the credentials in the code below to use a different setup)
 host: localhost
 user: ds2002
 password: UVA!1819
 
-3. you must have a database named 'employees' which can be setup at the following link:
+3. you must have a database named 'employees' loaded locally which can be setup at the following link:
 https://github.com/datacharmer/test_db
 
 '''
 
-# try:
-#     kaggle.api.authenticate()
-# except:
-#     print('Could not authenticate kaggle api')
-#     exit(1)
-# dataset = 'kyanyoga/sample-sales-data'
-# # fetch data via kaggle api
-# try:
-#     kaggle.api.dataset_download_files(dataset, path='./data/', unzip=True)
-# except:
-#     print('Could not fetch sales data from api')
-#     exit(1)
+try:
+    kaggle.api.authenticate()
+except:
+    print('Could not authenticate kaggle api')
+    exit(1)
+dataset = 'kyanyoga/sample-sales-data'
+# fetch sales data via kaggle api
+try:
+    kaggle.api.dataset_download_files(dataset, path='./data/', unzip=True)
+except:
+    print('Could not fetch sales data from api')
+    exit(1)
 
 # read sales data into df
 try:
@@ -43,7 +41,7 @@ except:
     print('Could not read sales_data_sample.csv')
     exit(1)
 
-# read the data into df
+# read ports data into df
 try:
     ports_df = pd.read_csv('data/port_data.csv')
 except:
@@ -58,6 +56,9 @@ except:
     print("DB read connection failed")
     exit(1)
 
+
+# here we select from employees, and natural join with salaries to add the salary as a column
+# this will be more efficient because future queries will not need to join with salaries
 query = "SELECT emp_no, birth_date, first_name, last_name, gender, hire_date, salary FROM employees NATURAL JOIN salaries ORDER BY salary DESC LIMIT 1000"
 try:
     employees_df = pd.read_sql(query, conn)
@@ -89,7 +90,6 @@ assert(len(employee_ids) == N, "Error, number of employee ids expected to be " +
 
 sales_df['EMPLOYEE_ID'] = sales_df['ORDERNUMBER'].apply(lambda x: employee_ids[(x * 7) % N])
 
-
 # trim unneeded columns to prep for sql insertion
 sales_df = sales_df[['ORDERNUMBER', 'QUANTITYORDERED', 'SALES', 'ORDERDATE', 'COUNTRY', 'EMPLOYEE_ID']]
 ports_df = ports_df[['Country', 'Vessels in Port', 'Port Name', 'Area Global']]
@@ -100,7 +100,6 @@ ports_df.columns = ['country', 'vessels', 'port_name', 'area_global']
 
 
 # write these tables to sql db
-
 def insert_sqlalchemy_dataframe(user_id, pwd, host_name, db_name, df, table_name):
     conn_str = f"mysql+pymysql://{user_id}:{pwd}@{host_name}/{db_name}"
     sqlEngine = sqlalchemy.create_engine(conn_str, pool_recycle=3600)
@@ -126,5 +125,3 @@ except:
 insert_sqlalchemy_dataframe(user, password, host, db, employees_df, 'employees')
 insert_sqlalchemy_dataframe(user, password, host, db, sales_df, 'sales')
 insert_sqlalchemy_dataframe(user, password, host, db, ports_df, 'ports')
-
-
--- project_queries.sql
+++ project_queries.sql
@@ -1,15 +1,27 @@
 use wahoo_nation_vehicles;
 
-SELECT AVG(quantity_ordered) AS "qty", country AS "Country"
-FROM sales GROUP BY country;
+-- query 1: What is the average quantity per order of vehicles in each country?
+SELECT AVG(quantity_ordered) AS "avg_qty", country AS "Country"
+FROM sales
+GROUP BY country;
 
-SELECT country, SUM(quantity_ordered) FROM sales GROUP BY country ORDER BY SUM(quantity_ordered) DESC;
+-- query 2: what is the total number of vehicles sold in each country?
+SELECT country, SUM(quantity_ordered) 
+FROM sales GROUP BY country 
+ORDER BY SUM(quantity_ordered) DESC;
 
-SELECT e.emp_no, e.first_name, e.last_name, SUM(s.quantity_ordered), e.salary, SUM(s.quantity_ordered) / e.salary AS "throughput"
+-- query 3: who were the top 3 the most productive employees, in terms of their sales per cost (productivity_score = quantity sold to salary ratio)?
+SELECT e.emp_no, e.first_name, e.last_name, SUM(s.quantity_ordered), e.salary, SUM(s.quantity_ordered) / e.salary AS "productivity_score"
 FROM sales s NATURAL JOIN employees e 
 GROUP BY e.emp_no, e.first_name, e.last_name
-ORDER BY SUM(s.quantity_ordered) / e.salary 
-DESC LIMIT 1;
+ORDER BY SUM(s.quantity_ordered) / e.salary DESC
+LIMIT 3;
+
+-- query 4: when was the first sale, and which employee made the sale?
+SELECT sale_date AS "first_sale_date", e.first_name, e.last_name
+FROM sales s NATURAL JOIN employees e
+ORDER BY sale_date ASC
+LIMIT 1;
 
 
 
